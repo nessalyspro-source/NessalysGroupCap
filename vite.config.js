5a4d8d06f30163bc3bd09618ee998bd5dd72e510
LNS D <nessalyspro@gmail.com>
2025-12-01 16:21:30 +0100

    Préparation déploiement GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Ensure assets resolve correctly on GitHub Pages
+  base: '/NessalysGroupCap/',
   plugins: [react()],
 })
